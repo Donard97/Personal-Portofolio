1817e36320ebde98da25bf9b9adb2ffe90794312
Donard97 <donardgolaj@gmail.com>
2021-08-04 23:15:57 +0200

    Delete javascript on script.js for modal

--- js/script.js
+++ js/script.js
@@ -5,15 +5,3 @@ menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
   mobile_menu.classList.toggle('is-active');
 });
-
-const modal = document.getElementById('modal');
-const openButton = document.getElementById('open-btn');
-const closeButton = document.getElementById('close-btn');
-
-openButton.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-closeButton.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
