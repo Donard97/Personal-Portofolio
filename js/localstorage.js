8f105877b66abe5db9f5cb833b6c69555ddc5612
Donard97 <donardgolaj@gmail.com>
2021-08-06 00:14:00 +0200

    Change javascript to complete requests

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,36 +1,41 @@
 /* eslint-disable max-len */
 /* eslint-disable func-style */
-const userFullName = document.getElementById('full-name');
-const userLastName = document.getElementById('Last-name');
-const userEmail = document.getElementById('email-address');
-const userMessage = document.getElementById('message');
 
-function setLocal () {
-  const localName = localStorage.getItem('full-name');
-  const localLastName = localStorage.getItem('Last-name');
-  const localemail = localStorage.getItem('email-address');
-  const localText = localStorage.getItem('message');
-  document.getElementById('full-name').value = localName;
-  document.getElementById('Last-name').value = localLastName;
-  document.getElementById('email-address').value = localemail;
-  document.getElementById('message').value = localText;
+
+const inputName = document.getElementById('full-name');
+const inputLastName = document.getElementById('Last-name');
+const inputEmail = document.getElementById('email-address');
+const inputMessage = document.getElementById('message');
+const formLocally = document.getElementById('form');
+
+function storageLocally () {
+  const localName = inputName.value;
+  const localLastName = inputLastName.value;
+  const localEmail = inputEmail.value;
+  const localMessage = inputMessage.value;
+
+  localStorage.setItem('full-name', localName);
+  localStorage.setItem('Last-name', localLastName);
+  localStorage.setItem('email-address', localEmail);
+  localStorage.setItem('message', localMessage);
 }
 
-function populateForm () {
-  localStorage.setItem('full-name', document.getElementById('full-name').value);
-  localStorage.setItem('Last-name', document.getElementById('Last-name').value);
-  localStorage.setItem('email-address', document.getElementById('email-address').value);
-  localStorage.setItem('message', document.getElementById('message').value);
-  setLocal();
+function preFillData () {
+  inputName.value += localStorage.getItem('full-name');
+  inputLastName.value += localStorage.getItem('Last-name');
+  inputEmail.value += localStorage.getItem('email-address');
+  inputMessage.value += localStorage.getItem('message');
 }
 
 if (localStorage.getItem('full-name')) {
-  populateForm();
+  preFillData();
 } else {
- setLocal();
+  storageLocally();
 }
 
-userFullName.onchange = populateForm;
-userLastName.onchange = populateForm;
-userEmail.onchange = populateForm;
-userMessage.onchange = populateForm;
+inputName.onchange = storageLocally;
+inputEmail.onchange = storageLocally;
+inputMessage.onchange = storageLocally;
+
+formLocally.addEventListener('submit', storageLocally, preFillData);
+
